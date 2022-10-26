8b46255bd0bf51fd27e569826f27f3290c23ddcb
Bacilio Alexander Bolaños Lima <alima@creditsesame.com>
2022-10-26 16:53:35 -0600

    Feature: Added logs for controllers

--- src/controllers/mathControllers.js
+++ src/controllers/mathControllers.js
@@ -1,28 +1,46 @@
 const operations = require("../operations");
 
+const log = (key, payload) => {
+ console.info(key, JSON.stringify(payload));
+}
+
 
 const sumar = (req, res, next) => {
     const { a, b} = req.params;
     const suma = operations.sumar(1*a,1*b);
-    console.log("sumar:",a,b,suma);
+    log("sumar", {operation: "sumar", valorA: a, valorB: b, resultado: suma, status:'success'});
+
     res.json({suma: suma});
 }
 
 
 const restar = (req, res, next) => {
     const { a, b} = req.params;
-    res.json({resta: a-b});
+    const resultado = a-b;
+    log("restar", {operation: "restar", valorA: a, valorB: b, resultado, status:'success'});
+
+    res.json({resta: resultado});
 }
 
 
 const multiplicar = (req, res, next) => {
     const { a, b} = req.params;
-    res.json({multiplicacion: a*b});
+    const resultado = a*b;
+    log("multiplicar", {operation: "multiplicar", valorA: a, valorB: b, resultado, status:'success'});
+    res.json({multiplicacion: resultado});
 }
 
 const dividir = (req, res, next) => {
     const {a, b} = req.params;
-    res.json({divicion: a/b});
+    if(b===0)
+    {
+        log("dividir", {operation: "dividir", valorA: a, valorB: b, resultado, status:'failed'} );
+        throw new Error("Imposible dividir por 0");
+    }
+    const resultado = a/b;
+
+    log("dividir", {operation: "dividir", valorA: a, valorB: b, resultado, status:'success'} );
+    res.json({division: a/b});
 }
 
 exports.sumar = sumar;
