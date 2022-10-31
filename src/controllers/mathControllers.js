3331afdc329fe56dac4d09d3e07355ae355bdf07
Bacilio Alexander Bolaños Lima <alima@creditsesame.com>
2022-10-31 15:52:41 -0600

    Update: Correct log function, to include project-name

--- src/controllers/mathControllers.js
+++ src/controllers/mathControllers.js
@@ -1,6 +1,6 @@
 const operations = require("../operations");
 
-const log = (key, payload) => {
+const log = (key="adminSys", payload) => {
  console.info(key, JSON.stringify(payload));
 }
 
@@ -8,7 +8,7 @@ const log = (key, payload) => {
 const sumar = (req, res, next) => {
     const { a, b} = req.params;
     const suma = operations.sumar(1*a,1*b);
-    log("sumar", {operation: "sumar", valorA: a, valorB: b, resultado: suma, status:'success'});
+    log( {operation: "sumar", valorA: a, valorB: b, resultado: suma, status:'success'});
 
     res.json({suma: suma});
 }
@@ -17,7 +17,7 @@ const sumar = (req, res, next) => {
 const restar = (req, res, next) => {
     const { a, b} = req.params;
     const resultado = a-b;
-    log("restar", {operation: "restar", valorA: a, valorB: b, resultado, status:'success'});
+    log({operation: "restar", valorA: a, valorB: b, resultado, status:'success'});
 
     res.json({resta: resultado});
 }
@@ -26,20 +26,20 @@ const restar = (req, res, next) => {
 const multiplicar = (req, res, next) => {
     const { a, b} = req.params;
     const resultado = a*b;
-    log("multiplicar", {operation: "multiplicar", valorA: a, valorB: b, resultado, status:'success'});
+    log({operation: "multiplicar", valorA: a, valorB: b, resultado, status:'success'});
     res.json({multiplicacion: resultado});
 }
 
 const dividir = (req, res, next) => {
     const {a, b} = req.params;
-    if(b===0)
+    if(b==="0")
     {
-        log("dividir", {operation: "dividir", valorA: a, valorB: b, resultado, status:'failed'} );
+        log( {operation: "dividir", valorA: a, valorB: b, resultado, status:'failed'} );
         throw new Error("Imposible dividir por 0");
     }
     const resultado = a/b;
 
-    log("dividir", {operation: "dividir", valorA: a, valorB: b, resultado, status:'success'} );
+    log( {operation: "dividir", valorA: a, valorB: b, resultado, status:'success'} );
     res.json({division: a/b});
 }
 
