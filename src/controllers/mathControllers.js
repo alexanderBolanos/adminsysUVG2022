23d5ba743a27dc6f0ae0ecd75da4d4280d446606
Bacilio Alexander Bolaños Lima <alima@creditsesame.com>
2022-11-07 17:59:09 -0600

    Update: Included Prettier

--- src/controllers/mathControllers.js
+++ src/controllers/mathControllers.js
@@ -1,53 +1,84 @@
 const operations = require("../operations");
 
 const log = (payload) => {
- console.info("adminSys2022-logs", JSON.stringify(payload));
-}
-
+    console.info("adminSys2022-logs", JSON.stringify(payload));
+};
 
 const sumar = (req, res, next) => {
-    const { a, b} = req.params;
-    const suma = operations.sumar(1*a,1*b);
-    log( {operation: "sumar", valorA: a, valorB: b, resultado: suma, status:'success'});
+    const { a, b } = req.params;
+    const suma = operations.sumar(1 * a, 1 * b);
+    log({
+        operation: "sumar",
+        valorA: a,
+        valorB: b,
+        resultado: suma,
+        status: "success"
+    });
 
-    res.json({suma: suma});
-}
-
+    res.json({ suma: suma });
+};
 
 const restar = (req, res, next) => {
-    const { a, b} = req.params;
-    const resultado = a-b;
-    log({operation: "restar", valorA: a, valorB: b, resultado, status:'success'});
+    const { a, b } = req.params;
+    const resultado = a - b;
+    log({
+        operation: "restar",
+        valorA: a,
+        valorB: b,
+        resultado,
+        status: "success"
+    });
 
-    res.json({resta: resultado});
-}
-
+    res.json({ resta: resultado });
+};
 
 const multiplicar = (req, res, next) => {
-    const { a, b} = req.params;
-    const resultado = a*b;
-    log({operation: "multiplicar", valorA: a, valorB: b, resultado, status:'success'});
-    res.json({multiplicacion: resultado});
-}
+    const { a, b } = req.params;
+    const resultado = a * b;
+    log({
+        operation: "multiplicar",
+        valorA: a,
+        valorB: b,
+        resultado,
+        status: "success"
+    });
+    res.json({ multiplicacion: resultado });
+};
 
 const dividir = (req, res, next) => {
-    const {a, b} = req.params;
+    const { a, b } = req.params;
 
-    if(a==="0")
-    {
-        log({operation: "dividir", valorA: a, valorB: b, resultado: null, status:'failed'} );
+    if (a === "0") {
+        log({
+            operation: "dividir",
+            valorA: a,
+            valorB: b,
+            resultado: null,
+            status: "failed"
+        });
         throw new Error("División no definida");
     }
-    if(b==="0")
-    {
-        log( {operation: "dividir", valorA: a, valorB: b, resultado: null, status:'failed'} );
+    if (b === "0") {
+        log({
+            operation: "dividir",
+            valorA: a,
+            valorB: b,
+            resultado: null,
+            status: "failed"
+        });
         throw new Error("Imposible dividir por 0");
     }
-    const resultado = a/b;
+    const resultado = a / b;
 
-    log( {operation: "dividir", valorA: a, valorB: b, resultado, status:'success'} );
-    res.json({division: resultado});
-}
+    log({
+        operation: "dividir",
+        valorA: a,
+        valorB: b,
+        resultado,
+        status: "success"
+    });
+    res.json({ division: resultado });
+};
 
 exports.sumar = sumar;
 exports.dividir = dividir;
