4b784fee01dcab64501b0f8d6df537145ff0aef5
Bacilio Alexander Bolaños Lima <alima@creditsesame.com>
2022-11-09 17:33:15 -0600

    Testing with Husky

--- src/tests/calculator.test.js
+++ src/tests/calculator.test.js
@@ -1,22 +1,21 @@
-const { restar } = require('../controllers/mathControllers');
-const {sumar} = require('../operations');
+const { restar } = require("../controllers/mathControllers");
+const { sumar } = require("../operations");
 
-test("Sumando integer (1) más integer (2): ", ()=>{
-    expect(sumar(1,2)).toBe(3);
+test("Sumando integer (1) más integer (2): ", () => {
+    expect(sumar(1, 2)).toBe(3);
 });
 
-test("Sumando real(1.5) y real(2.5):", ()=>{
+test("Sumando real(1.5) y real(2.5):", () => {
     expect(sumar(1.5, 2.5)).toBe(4);
 });
 
-test("Sumando nulls:", ()=>{
+test("Sumando nulls:", () => {
     expect(sumar(null, null)).toBe(null);
 });
 
-
 function prueba3() {
     //estas funciones deben ser testeables: no func. asincronas
-    expect(sumar('a','b')).toBe(null);
+    expect(sumar("a", "b")).toBe(null);
 }
 
 test("Prueba sumando caracteres:", prueba3);
